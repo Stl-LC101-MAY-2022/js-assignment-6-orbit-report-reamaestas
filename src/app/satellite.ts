70a746484738b7a836addd9fc71c239f4fde18a4
reamaestas <reamaestas@yahoo.com>
2022-08-17 14:31:53 -0500

    closed everything and trying again

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -18,15 +18,15 @@ export class Satellite {
 		return this.type === 'Space Debris'
    } 
 
-   	numOfSat(): number {
-		let numDebris = 0;
-		for (let i=0; i<=this.type.length; i++) {
-			if (this.type[i] === 'Space Debris') {
-				numDebris =+ i;
-			}
-		}
-		return (this.type.length - numDebris);
-	}
+   	// numOfSat(): number {
+	// 	let numDebris = 0;
+	// 	for (let i=0; i<=this.type.length; i++) {
+	// 		if (this.type[i] === 'Space Debris') {
+	// 			numDebris =+ i;
+	// 		}
+	// 	}
+	// 	return (this.type.length - numDebris);
+	// }
 
 // TODO 3a: fix isSpaceDebris check
 
