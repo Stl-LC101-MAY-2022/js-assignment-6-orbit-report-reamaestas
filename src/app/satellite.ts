b69f3f30f120aadb461e43c98826b43fc336e654
reamaestas <reamaestas@yahoo.com>
2022-08-17 13:54:47 -0500

    end of part 4

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -5,7 +5,7 @@ export class Satellite {
 	launchDate: string;
 	orbitType: string;
 	operational: boolean;
-
+	
 	constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
 		this.name = name;
 		this.type = type;
@@ -15,9 +15,19 @@ export class Satellite {
    }
 	
 	isSpaceDebris(): boolean {
-		return true;
-   }
+		return this.type === 'Space Debris'
+   } 
 
-}
+   	numOfSat(): number {
+		let numDebris = 0;
+		for (let i=0; i<=this.type.length; i++) {
+			if (this.type[i] === 'Space Debris') {
+				numDebris =+ i;
+			}
+		}
+		return (this.type.length - numDebris);
+	}
 
 // TODO 3a: fix isSpaceDebris check
+
+}
